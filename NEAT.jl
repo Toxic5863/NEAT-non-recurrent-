66dedad82a3e3c104aa924aee9a3fb0ca1071fe1
Toxic5863 <hewitt.alexander.1998@gmail.com>
2021-04-25 21:42:32 -0400

    added phenotype representation and mutation scheme for adding nodes

--- NEAT.jl
+++ NEAT.jl
@@ -1,15 +1,18 @@
-# This is a trial run of using the NEAT algorithm to train a neural network
 import LinearAlgebra
 
-# →(x, y) = getfield(x, y)    # custom operator for getting fields of structs
+isnumber(x) = isa(x, Number)
+is_sensor(x) = getfield(x, :class) == 'S'
+is_output(x) = getfield(x, :class) == 'O'
+get_in(x) = getfield(x, :in)
+is_active(x) = getfield(x, :activity) == true
 
-
-struct node_gene
+mutable struct node_gene
     number::Int
     class::Char
+    innovation_number::Int
 end
 
-struct connection_gene
+mutable struct connection_gene
     in::Int
     out::Int
     weight::Float64
@@ -17,7 +20,7 @@ struct connection_gene
     innovation_number::Int
 end
 
-struct genome
+mutable struct genome
     nodes::Array
     connections::Array
 end
@@ -30,16 +33,100 @@ function sigmoid(x)
     return 1 / (1 + ℯ^-x)
 end
 
-is_sensor(x) = getfield(x, :class) == 'S'
-is_active(x) = getfield(x, :activity) == true
-get_in(x) = getfield(x, :in)
-
 function propagate(x::genome, y::Array)
-    # input_layer = filter(is_sensor, x.nodes)
-    active_connections = filter(is_active, x.connections)
-    sorted_active_connections = sort(active_connections, by=get_in)
+    # the output node must be deteremined to be used as the starting point
+    # for evaluating the output of the phenotype in a top-down manner
+    output_node_number = filter(is_output, x.nodes)[1].number
+
+    # using a directed adjacency matrix to model the phenotype
+    # weighted edge i to j is represented as matrix[j, i] = weight
+    node_count = length(x.nodes)
+    weighted_adjacency_matrix = Array{Any}(undef, node_count, node_count)
+
+    # setting the default entry to "e" for empty. this way, if an edge's
+    # weight is not a number, we know to ignore it
+    fill!(weighted_adjacency_matrix, 'e')
+
+    # populating the adjacency matrix with weights
+    for a in x.connections
+        weighted_adjacency_matrix[a.out, a.in] = a.weight
+    end
+
+    # using top-down recursion to navigate the adjacency matrix
+    # start at output node and navigate backwards along all edges with weights
+    # until leaf nodes are arrived at. if leaves are sensor nodes, they return
+    # the value of their corresponding inputs
+    network_output = calculate_output(output_node_number, weighted_adjacency_matrix, y)
+end
+
+# w = weight of edge, x = origin vertex of edge, y = graph, z = inputs
+function evaluate_connection(w, x, y, z)
+    if isa(w, Number)
+        return sigmoid(w * evaluate_row(x, y, z))
+    else
+        return 0
+    end
+end
+
+# x = node number, y = adjacency matrix, z = inputs
+function evaluate_row(x, y, z)
+    if sum(isnumber.(y[x, :])) == 0
+        if x <= length(z)
+            return z[x]
+        else
+            return 0
+        end
+    else
+        input_sum = 0
+        for a in 1:length(y[x, :])
+            input_sum += evaluate_connection(y[x, a], a, y, z)
+        end
+        return input_sum
+    end
+end
+
+# x = node number, y = adjacency matrix, z = inputs
+function calculate_output(x, y, z)
+    return sigmoid(evaluate_row(x, y, z))
+end
+
+# the innovation count i must be set equal to the output of mutations
+function add_node(x::genome, i::Int)
+    found_active_connection = false
+    target_connection_index = rand(1:length(x.connections))
+    while found_active_connection == false
+        if x.connections[target_connection_index].activity == true
+            found_active_connection = true
+            target_connection_index = rand(1:length(x.connections))
+        end
+    end
+    node_count = length(x.nodes)
+    node_count += 1
+
+    x.connections[target_connection_index].activity = false
+    edge_out = x.connections[target_connection_index].out
+    edge_in = x.connections[target_connection_index].in
+
+    new_node = node_gene(node_count, 'H', i)
+    x.nodes = hcat(x.nodes, new_node)
+
+    new_connection = connection_gene(edge_in, node_count, 1, true, i)
+    x.connections = hcat(x.connections, new_connection)
+
+    new_connection = connection_gene(node_count, edge_out, 1, true, i)
+    x.connections = hcat(x.connections, new_connection)
+    return i
+end
+
+# the innovation count i must be set equal to the output of mutations
+function add_edge(i)
 end
 
 
-# sample genome for debugging
-a = genome([node_gene(1, 'S') node_gene(2, 'S') node_gene(3, 'O')], [connection_gene(1, 3, 1, true, 1) connection_gene(2, 3, 1, true, 1)])
+i = 1
+# sample genome and inputs for debugging
+nodes = [node_gene(1, 'S', i) node_gene(2, 'H', i) node_gene(3, 'H', i) node_gene(4, 'O', i)]
+connections = [connection_gene(1, 2, 1, true, 1) connection_gene(2, 3, 1, true, 2) connection_gene(3, 4, 1, true, 3)]
+a = genome(nodes, connections)
+input = [1]
+# node 4 = w*sigmoid(sum(w*sigmoid()))
